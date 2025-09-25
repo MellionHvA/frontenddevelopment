4641c4b1e1b5c404f8721808f44925b088f0c330
MellionHvA <melih.celik@hva.nl>
2025-09-25 14:40:28 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,10 @@
 // JavaScript Document
 const menuButton = document.querySelector("header button:nth-of-type(2)");
 const mainNav = document.querySelector("header nav:nth-of-type(1)");
+const overlay = document.querySelector(".overlay");
 
 menuButton.addEventListener("click", () => {
   menuButton.toggleAttribute("data-open");
   mainNav.toggleAttribute("data-open");
+  overlay.toggleAttribute("data-open");
 });
